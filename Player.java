9e2c877b0105f703e91d48875220d8a4afe3ed32
F1D021016_Zila <128133261+anzilawhyn@users.noreply.github.com>
2023-12-14 21:29:26 +0800

    Delete Player.java

--- Player.java
+++
@@ -1,9 +0,0 @@
-public abstract Player {
-  private double health;
-  private String name;
-  private boolean alive;
-  private long move;
-  
-  public Player(String name, double health, boolean alive, long move){
-    this.name = name;
-    this.health = health;
