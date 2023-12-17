f0b22708d690432cd8f17d5758ca4b774bfc8aee
dimas afryzal hanan <dimasafryzalhanan@gmail.com>
2023-12-17 14:23:45 +0800

    Perubahan class dirver

--- Driver.java
+++ Driver.java
@@ -1,6 +1,7 @@
 public class Driver {
 
     public static void main(String[] args) {
+	ConnectDatabase connect = new ConnectDatabase();
 	Player player = new Player("Galih", 6.0, true, 250);
 	GUI gui = new GUI(player);
 	gui.startMenu();
